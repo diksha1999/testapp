14c15309fe116ca1d96f522fd3b12514e968a97b
Diksha Sharma <33152237+diksha1999@users.noreply.github.com>
2025-04-01 10:56:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, OpenShift! 🚀 This app is deployed using Tekton."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
